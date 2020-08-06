ad3c5a5a3d33ce6884ce3970b7f6a40f35342a09
payflowpa <payflowpa@comune.bari.it>
2020-08-06 12:15:36 +0200

    Initial commit

---
+++ sorgenti/riconciliazione/mypivot/RELEASE/update/mypivot/from-5.4.2.2-to-5.4.2.3/batch/jobs/BatchFlussoTesoreriaF2K/Main_Manage_Flusso_Tesoreria_F2K/Main_Manage_Flusso_Tesoreria_F2K_run.bat
@@ -0,0 +1,3 @@
+%~d0
+ cd %~dp0
+ java -Xms256M -Xmx1024M -cp ../lib/commons-lang-2.6.jar;../lib/talend-oracle-timestamptz.jar;../lib/ojdbc6.jar;../lib/dom4j-1.6.1.jar;../lib/talend_file_enhanced_20070724.jar;../lib/tns.jar;../lib/postgresql-9.3-1104-jdbc41.jar;../lib/commons-collections-3.2.jar;../lib/log4j-1.2.15.jar;../lib/talend-log4j-config-util.jar;../lib/advancedPersistentLookupLib-1.0.jar;../lib/trove.jar;../lib/talendcsv.jar;../lib/jboss-serialization.jar;../lib/systemRoutines.jar;../lib/userRoutines.jar;.;main_manage_flusso_tesoreria_f2k_0_1.jar;allinea_f2k_0_1.jar;log_nagios_0_1.jar;manage_log_0_1.jar; manage_flusso_tesoreria_f2k.main_manage_flusso_tesoreria_f2k_0_1.Main_Manage_Flusso_Tesoreria_F2K %* 
