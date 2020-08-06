657be8847764519dc9cce1cd3af5b2cc3a363a7c
payflowpa <payflowpa@comune.bari.it>
2020-08-06 12:13:14 +0200

    Initial commit

---
+++ sorgenti/riconciliazione/mypivot/RELEASE/update/mypivot/from-5.3.6.x-to-5.4.0/batch/jobs/BatchAllineaDatabase/Main_Allinea_Database/Main_Allinea_Database_run.bat
@@ -0,0 +1,3 @@
+%~d0
+ cd %~dp0
+ java -Xms256M -Xmx1024M -cp ../lib/commons-lang-2.6.jar;../lib/dom4j-1.6.1.jar;../lib/talend_file_enhanced_20070724.jar;../lib/postgresql-9.3-1104-jdbc41.jar;../lib/commons-collections-3.2.jar;../lib/log4j-1.2.15.jar;../lib/talend-log4j-config-util.jar;../lib/advancedPersistentLookupLib-1.0.jar;../lib/trove.jar;../lib/talendcsv.jar;../lib/jboss-serialization.jar;../lib/systemRoutines.jar;../lib/userRoutines.jar;.;main_allinea_database_0_1.jar;allinea_0_1.jar;log_nagios_0_1.jar;manage_log_0_1.jar; allinea_database.main_allinea_database_0_1.Main_Allinea_Database --context=Default %* 
